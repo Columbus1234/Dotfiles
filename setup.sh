f88c89a660b1bf289167d6512f98846338da3612
Columbus1234 <christoffer019283@gmail.com>
2020-06-25 20:19:41 +0200

    Adding my dotfiles to a repo, next up is script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Sample
+#ln -s ${BASEDIR}/path/to/dotfile/or/directory ~/path/to/default/location/of/said/file
+
+
+
+# Polybar
+ln -s ${BASEDIR}/polybar/ ~/.config/
+
+# zsh
+ln -s ${BASEDIR}/zshrc ~/.zshrc
+
+# Alacritty
+ln -s ${BASEDIR}/alacritty/ ~/.config/
